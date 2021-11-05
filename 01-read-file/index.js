4999366a356e0dde22282282a317a87af5472aa5
JacksonBaltramovich <e.baltramovich@mail.ru>
2021-11-05 18:14:25 +0300

    feat: finished 02-write-file test

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 
-const myPath = path.join(__dirname, 'text.txt');
-const myReadText = fs.createReadStream(__dirname + '/text.txt', 'utf-8');
+c
+const myReadText = fs.createReadStream(myPath, 'utf-8');
 
 myReadText.on('data', function(chunk) {
     console.log(chunk);
