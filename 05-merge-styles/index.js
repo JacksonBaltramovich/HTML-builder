0f4ff0b482717927eafd7e525c0cf7742b5a146c
JacksonBaltramovich <e.baltramovich@mail.ru>
2021-11-09 01:41:35 +0300

    delete bundle.css 04 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -23,7 +23,6 @@ async function mergeStyles() {
                 fs.readFile(sourcePath, 'utf-8', (err, content) => {
                     if (err) throw err;
 
-
                     fs.appendFile(bundlePath, content, err => {
                         if(err) throw err;
                     })
